37f8bed085bfe8379816681811fb9c9c4a265c3c
tamadekk <186jnd@gmail.com>
2024-05-25 20:13:57 +0200

    feat: add Input component

---
+++ src/common/Input/Input.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface Props {
+	icon?: string;
+	name: string;
+	type: string;
+	id: string;
+	placeHolder: string;
+	required: boolean;
+}
+
+const Input = (props: Props) => {
+	return (
+		<div className='relative'>
+			<div className='absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none'>
+				{props.icon && <img src={props.icon} alt='icon' className='w-4 h-4' />}
+			</div>
+			<input
+				className='w-full ps-10 p-2.5 bg-bgInput rounded-md font-poppins font-extralight text-sm'
+				name={props.name}
+				type={props.type}
+				id={props.id}
+				placeholder={props.placeHolder}
+			/>
+		</div>
+	);
+};
+
+export default Input;
