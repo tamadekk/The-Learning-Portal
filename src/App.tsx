085db29a6b8e009fc808a75b78f5e7bb715ced6b
tamadekk <186jnd@gmail.com>
2024-07-23 22:10:47 +0200

    feat: add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import './App.scss';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+
+import Login from './components/login/Login';
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>Hello World!</h1>
-		</div>
+		<>
+			<Header />
+			<Routes>
+				<Route path='*' element={<Navigate to='/' />} />
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
+		</>
 	);
 }
 
