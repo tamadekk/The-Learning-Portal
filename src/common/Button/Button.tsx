8d343534ebdbbb9782c18b497290c5f2c9e2a23d
tamadekk <186jnd@gmail.com>
2024-08-14 14:38:07 +0200

    feat: add disabled state for button

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 interface ButtonProps {
 	message: string;
+	disabled?: boolean;
+	onClick?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
 	return (
-		<button className='w-full h-10 bg-buttonPrime hover:bg-buttonPrimeHover focus:bg-buttonPrimePressed text-white rounded-md'>
+		<button
+			className='w-full h-10 bg-buttonPrime hover:bg-buttonPrimeHover focus:bg-buttonPrimePressed disabled:bg-slate-600 text-white rounded-md'
+			disabled={props.disabled}
+			onClick={props.onClick}
+		>
 			{props.message}
 		</button>
 	);
