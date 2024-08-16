5876e8f855999c5b625f18fbe861769508570435
tamadekk <186jnd@gmail.com>
2024-08-16 13:09:16 +0200

    fix: wrong layout for background image in JoinUs section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,13 +6,14 @@ import { TBSVideoPic, joinUsBGPic } from '../../assets/index';
 
 export const Home = () => {
 	return (
-		<section>
-			<div className='flex flex-col items-center mt-24 mb-14'>
-				<div className='mb-10'>
+		<>
+			<div className='flex flex-col items-center mt-24 gap-14'>
+				<div className=''>
 					<h1 className='text-center text-5xl text-headerTextColor font-montserrat font-semibold'>
 						Let's start learning
 					</h1>
 				</div>
+
 				<div className='text-center w-2/5'>
 					<p className='text-xl text-nonPrimeText font-montserrat leading-relaxed'>
 						Welcome to Learn Platform - where every day is a day to learn. Dive
@@ -20,29 +21,32 @@ export const Home = () => {
 						for a successful tomorrow. Happy learning!
 					</p>
 				</div>
-			</div>
-			<div className='flex justify-center'>
-				<img src={TBSVideoPic} alt='Promotional video thumbnail' />
-			</div>
 
-			<div className='flex justify-center mt-24 w-screen'>
+				<div className='flex justify-center'>
+					<img src={TBSVideoPic} alt='Promotional video thumbnail' />
+				</div>
+
 				<div
-					className='p-8 rounded-lg shadow-lg max-w-xl text-center w-screen'
+					className='flex flex-col items-center justify-center mt-24 w-3/4 h-96 bg-cover bg-center mb-14'
 					style={{
-						backgroundImage: `url(${joinUsBGPic})`,
-						backgroundSize: 'cover',
+						backgroundImage: `url(${joinUsBGPic}) `,
+						backgroundSize: 'contain',
+						backgroundRepeat: 'no-repeat',
 					}}
 				>
-					<h1 className='text-4xl font-bold text-indigo-700 mb-4'>Join us</h1>
-
-					<p className='text-gray-600 mb-6'>
-						Qui ut exercitation officia proident enim non tempor tempor ipsum ex
-						nulla ea adipisicing sit consequat enim elit cupidatat o
-					</p>
+					<div className='flex flex-col items-center w-1/3'>
+						<h1 className='text-4xl font-bold text-indigo-700 mb-4 text-nowrap'>
+							Join us
+						</h1>
+						<p className='text-gray-600 mb-6 text-center'>
+							Qui ut exercitation officia proident enim non tempor tempor ipsum
+							ex nulla ea adipisicing sit consequat enim elit cupidatat o
+						</p>
+					</div>
 					<Button message='Join us' />
 				</div>
 			</div>
-		</section>
+		</>
 	);
 };
 
