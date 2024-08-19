3c1a4914cec5b335fc4991c4893f3212763fe9b2
tamadekk <186jnd@gmail.com>
2024-08-19 17:19:14 +0200

    echo: add new icons from fons-icon pack. add a new transparent logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,17 +1,25 @@
-import headerLogo from './Header/HeaderLogo.png';
+import logo from './logo.png';
 import userIcon from './Login/userIcon.svg';
 import padlockIcon from './Login/padlockIcon.svg';
 import eyeOn from './Login/eyeIcon.svg';
 import eyeOff from './Login/eyeOffIcon.svg';
 import TBSVideoPic from './Home/TBS-Video.png';
 import joinUsBGPic from './Home/joinUsBG.png';
+import emailIcon from './Home/email.svg';
+import twitterIcon from './Footer/x-twitterIcon.svg';
+import youtubeIcon from './Footer/youtubeIcon.svg';
+import facebookIcon from './Footer/facebookIcon.svg';
 
 export {
-	headerLogo,
+	logo,
 	userIcon,
 	padlockIcon,
 	eyeOn,
 	eyeOff,
 	TBSVideoPic,
 	joinUsBGPic,
+	emailIcon,
+	twitterIcon,
+	youtubeIcon,
+	facebookIcon,
 };
