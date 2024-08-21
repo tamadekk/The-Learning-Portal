23403c3b1ad84b49159bbe8475025608063ce95f
tamadekk <186jnd@gmail.com>
2024-08-21 14:56:30 +0200

    feat: make Header component responsive

---
+++ src/components/Header/NavBar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+interface Links {
+	href: string;
+	label: string;
+}
+
+interface NavBarProps {
+	links: Links[];
+}
+
+const NavBar = (props: NavBarProps) => {
+	const pathName = useLocation().pathname;
+	return (
+		<>
+			{props.links.map((item) => (
+				<nav key={item.href}>
+					<a
+						href={item.href}
+						className={`block p-4 ${
+							pathName === item.href
+								? 'border-l-8 border-purpleText text-purpleText font-bold sm:border-none sm:text-inherit sm:font-normal'
+								: ''
+						}`}
+					>
+						{item.label}
+					</a>
+				</nav>
+			))}
+		</>
+	);
+};
+
+export default NavBar;
