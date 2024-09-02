d7ffd47be432ac23e0d982b7a351eec129d2355c
tamadekk <186jnd@gmail.com>
2024-09-02 11:17:14 +0200

    fix: remove extra padding for mobile devices

--- src/components/JoinUs/JoinUsBox.tsx
+++ src/components/JoinUs/JoinUsBox.tsx
@@ -11,7 +11,7 @@ interface JoinUsBoxProps {
 const JoinUsBox = (props: JoinUsBoxProps) => {
 	return (
 		<div className='flex justify-center items-center p-16 rounded'>
-			<div className='flex flex-col gap-5 items-center bg-neutral-50 rounded-xl pl-14 md:flex-row w-full max-w-screen-lg'>
+			<div className='flex flex-col gap-5 pt-6 items-center md:pl-14 min-w-96 sm:w-full md:flex-row lg:pt-0 max-w-screen-lg bg-neutral-50 rounded-xl'>
 				<div className='flex flex-col text-center justify-between gap-2 md:w-1/2 md:text-left'>
 					<h2 className='text-4xl text-headerTextColor font-bold'>
 						Register as {props.role}
