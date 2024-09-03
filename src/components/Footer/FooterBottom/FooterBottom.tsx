7b6fe8f895c514feeea57d8d7d0f15cca962c75c
tamadekk <186jnd@gmail.com>
2024-09-03 15:44:36 +0200

    feat: use reusable Selector component insteaf of LanguageSelector

--- src/components/Footer/FooterBottom/FooterBottom.tsx
+++ src/components/Footer/FooterBottom/FooterBottom.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FOOTER_SOCIAL_LINKS } from 'src/constants/Footer/constants';
-import LanguageSelector from './LanguageSelector/LanguageSelector';
+import {
+	FOOTER_SOCIAL_LINKS,
+	LANGUAGE_OPTIONS,
+} from 'src/constants/Footer/constants';
+
 import LegalLinks from './LegalLinks/LegalLinks';
+import Selector from 'src/common/Selector/Selector';
 
 const FooterBottom = () => {
+	const [selectedLanguage, setSelectedLanguage] = useState('');
+
+	const handleLanguageChange = (event) => {
+		setSelectedLanguage(event.target.value);
+	};
 	return (
 		<aside className='container mx-auto mt-10 border-t border-gray-200 pt-4 grid grid-cols-1 md:grid-cols-3 gap-4 text-center px-4 text-nowrap'>
-			<LanguageSelector />
+			<Selector
+				id='language-select'
+				name='language'
+				options={LANGUAGE_OPTIONS}
+				value={selectedLanguage}
+				onChange={handleLanguageChange}
+				className='w-1/3'
+			/>
 			<LegalLinks />
 			<div className='flex justify-center md:justify-end space-x-4'>
 				{FOOTER_SOCIAL_LINKS.map((item, index) => (
