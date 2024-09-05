ba086ea32ae1d51ce976ee229e1d35c1fe07d182
tamadekk <186jnd@gmail.com>
2024-09-05 11:58:46 +0200

    fix: navigation link color should be purple if user is on this page

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -20,7 +20,7 @@ const NavBar = (props: NavBarProps) => {
 						href={item.href}
 						className={`block p-4 ${
 							pathName === item.href
-								? 'border-l-8 border-purpleText text-primary-500 font-bold sm:border-none sm:text-inherit sm:font-normal'
+								? 'border-l-8 border-primary-500 text-primary-500 font-bold sm:border-none sm:font-normal'
 								: ''
 						}`}
 					>
